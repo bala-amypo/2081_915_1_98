a81a24a667fe3188fd9f90cf0e8fa59840fee63b
bala-amypo <bala@amypo.in>
2025-12-21 15:40:30 +0000

    User commit

--- src/main/java/com/example/demo/service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/ServiceImpl/UserServiceImpl.java
@@ -3,24 +3,36 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository repository;
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository repository) {
-        this.repository = repository;
-    }
-
+    @Override
     public User saveUser(User user) {
-        return repository.save(user);
+        return userRepository.save(user);
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return repository.findAll();
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElse(null); // returns null if not found
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
