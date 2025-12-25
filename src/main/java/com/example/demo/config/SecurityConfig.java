49a570ffa47bef1f95c56b8ed22b906eb0ca0164
bala-amypo <bala@amypo.in>
2025-12-25 07:40:17 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,30 @@
-package com.example.demo.config;
+package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**").permitAll()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
