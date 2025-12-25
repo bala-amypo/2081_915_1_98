c3b70dae594390efe64c39047d11e65471a2600b
bala-amypo <bala@amypo.in>
2025-12-25 08:52:17 +0000

    User commit

--- src/main/java/com/example/demo/service/MicroLessonService.java
+++ src/main/java/com/example/demo/service/MicroLessonService.java
@@ -1,12 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.MicroLesson;
-
 import java.util.List;
 
 public interface MicroLessonService {
-
-    MicroLesson save(MicroLesson lesson);
-
-    List<MicroLesson> findByCourseId(Long courseId);
+    MicroLesson createMicroLesson(MicroLesson microLesson);
+    MicroLesson getMicroLesson(Long id);
+    List<MicroLesson> getAllMicroLessons();
+    MicroLesson updateMicroLesson(Long id, MicroLesson microLesson);
+    void deleteMicroLesson(Long id);
 }
